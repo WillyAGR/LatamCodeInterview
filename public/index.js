54b09ae00b8e7b545348fb37514885111dfd8e62
William Gómez <105875015+WillyAGR@users.noreply.github.com>
2023-04-21 17:31:32 -0600

    Dia 2
    Desarrollo de blog y servicios

---
+++ public/index.js
@@ -0,0 +1,19 @@
+var images = [
+    "../public/img/banner-1.png",
+    "../public/img/banner-2.png",
+    "../public/img/banner-3.png"
+  ];
+  
+  var currentImage = 0;
+
+  setInterval(changeBackgroundImage, 5000);
+
+  function changeBackgroundImage() {
+    var heroSection = document.querySelector(".hero-section");
+    currentImage++;
+    if (currentImage >= images.length) {
+      currentImage = 0;
+    }
+    heroSection.style.backgroundImage = "url('" + images[currentImage] + "')";
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     colors:{
       'colorNavBar': '#303030;',
       'colorTextNavbar': '#ffffff;'
+      
     }
 
   },
